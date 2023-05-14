854861c3f5b24167d20e357700261bd7605df803
jennifer <gwyjennifer@gmail.com>
2023-05-14 12:07:42 +0800

    initial commit

---
+++ department-service/src/main/java/net/jennglebell/departmentservice/service/DepartmentService.java
@@ -0,0 +1,9 @@
+package net.jennglebell.departmentservice.service;
+
+import net.jennglebell.departmentservice.dto.DepartmentDto;
+
+public interface DepartmentService {
+    DepartmentDto saveDepartment(DepartmentDto departmentDto);
+
+    DepartmentDto getDepartmentByCode(String code);
+}
